3fd672b7fc504c7ece15f78ec76012917004092c
kocip <kocip@FREAKY>
2023-06-05 13:41:56 +0300

    Revert ""project skeleton""
    This reverts commit 2a9680b6e1443dd20a0740a587a715f24a8a3ca3.

--- Web.Forum/Services/IAdminService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web.Forum.Services
-{
-    public interface IAdminService
-    {
-        void BlockUser(int adminId, int userId);
-        void UnblockUser(int adminId, int userId);
-        void DeletePost(int adminId, int postId);
-    }
-}
