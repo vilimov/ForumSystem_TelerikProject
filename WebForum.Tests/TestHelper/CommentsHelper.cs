8860eae25458406890bcba5c1fdbb92493091d41
milkotomov <el_loco@abv.bg>
2023-06-26 09:35:36 +0300

    Some Comment Tests

---
+++ WebForum.Tests/TestHelper/CommentsHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebForum.Models;
+
+namespace WebForum.Tests.TestHelper
+{
+    public class CommentsHelper
+    {
+        public static Comment GetTestComment()
+        {
+            return new Comment
+            {
+                Id = 1,
+                Content = "TestContent",
+                CreatedAt = DateTime.Now,
+                Likes = 5,
+                AutorId = 1,
+                PostId = 1
+            };
+        }
+
+        public static List<Comment> GetTestComments()
+        {
+            return new List<Comment>
+            {
+                new Comment 
+                {
+                Id = 1,
+                Content = "TestContent 1",
+                CreatedAt = DateTime.Now,
+                Likes = 1,
+                AutorId = 1,
+                PostId = 1
+                },
+
+                new Comment
+                {
+                Id = 2,
+                Content = "TestContent 2",
+                CreatedAt = DateTime.Now,
+                Likes = 2,
+                AutorId = 2,
+                PostId = 2
+                },
+
+                new Comment
+                {
+                Id = 3,
+                Content = "TestContent 3",
+                CreatedAt = DateTime.Now,
+                Likes = 3,
+                AutorId = 3,
+                PostId = 3
+                }
+            };
+        }
+    }
+}
