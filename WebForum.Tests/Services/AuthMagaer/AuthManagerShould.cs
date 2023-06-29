a96045e1f1f0e3dc8a439ee824d9b4436d912c17
mila.mitova@gmail.com <d]e=_cW)n[8-JZ>
2023-06-29 16:51:47 +0300

    added some more Unit tests

---
+++ WebForum.Tests/Services/AuthMagaer/AuthManagerShould.cs
@@ -0,0 +1,119 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebForum.Helpers.Authentication;
+using WebForum.Helpers.Exceptions;
+using WebForum.Models;
+using WebForum.Services;
+
+namespace WebForum.Tests.Services.AuthMagaer
+{
+    [TestClass]
+    public class AuthManagerShould
+    {
+        [TestMethod]
+        public void Return_CorrectUser_WhenParamsAreValid()
+        {
+            //Arrange
+            User testUser = TestHelper.UsersHelper.GetTestUser();
+
+            var userServiceMock = new Mock<IUserServices>();
+            userServiceMock.Setup(userService => userService
+                .GetByUsername(testUser.Username))
+                .Returns(testUser);
+
+            var sut = new AuthManager(userServiceMock.Object);
+
+            // Act
+            User actualUser = sut.TryGetUser($"{testUser.Username}:Cleopatra");
+
+            //Assert
+            Assert.AreSame(testUser, actualUser);
+
+        }
+
+        [TestMethod]
+        public void Throw_WhenCredentialsAreInvalid()
+        {
+            //Arrange
+            User testUser = TestHelper.UsersHelper.GetTestUser();
+
+            var userServiceMock = new Mock<IUserServices>();
+            userServiceMock.Setup(userService => userService
+                .GetByUsername(testUser.Username))
+                .Returns(testUser);
+
+            var sut = new AuthManager(userServiceMock.Object);
+
+            // Act
+            //User actualUser = sut.TryGetUser($"{testUser.Username}:Cleopatra");
+
+            //Assert
+            Assert.ThrowsException<UnauthorizedOperationException>(() => sut.TryGetUser($"{testUser.Username}:Cleopatr"));
+
+        }
+        [TestMethod]
+        public void Throw_WhenCredentialsAreEmpty()
+        {
+            //Arrange
+            User testUser = TestHelper.UsersHelper.GetTestUser();
+
+            var userServiceMock = new Mock<IUserServices>();
+            userServiceMock.Setup(userService => userService
+                .GetByUsername(testUser.Username))
+                .Returns(testUser);
+
+            var sut = new AuthManager(userServiceMock.Object);
+
+            // Act
+            //User actualUser = sut.TryGetUser($"{testUser.Username}:Cleopatra");
+
+            //Assert
+            Assert.ThrowsException<InvalidPasswordException>(() => sut.TryGetUser($"{testUser.Username}Cleopatr"));
+
+        }
+        [TestMethod]
+        public void Throw_WhenUsernameNotFound()
+        {
+            //Arrange
+            User testUser = TestHelper.UsersHelper.GetTestUser();
+
+            var userServiceMock = new Mock<IUserServices>();
+            userServiceMock.Setup(userService => userService
+                .GetByUsername(testUser.Username))
+                .Returns(testUser);
+
+            var sut = new AuthManager(userServiceMock.Object);
+
+            // Act
+            //User actualUser = sut.TryGetUser($"{testUser.Username}:Cleopatra");
+            
+            //Assert
+            Assert.ThrowsException<UnauthorizedOperationException>(() => sut.TryGetUser($"wrongUsername:Cleopatra"));
+
+        }
+
+        [TestMethod]
+        public void Return_ValidSalt()
+        {
+            // Act
+            string salt = AuthManager.GenerateSalt();
+
+            // Assert
+            Assert.IsFalse(string.IsNullOrEmpty(salt), "Salt should not be null or empty.");
+            Assert.AreEqual(24, salt.Length, "Salt should have a length of 24 characters.");
+            Assert.IsTrue(IsValidBase64String(salt), "Salt should only contain valid base64 characters.");
+        }
+
+        private static bool IsValidBase64String(string input)
+        {
+            // Check if the input string contains only valid base64 characters
+            return input.All(c => c == '+' || c == '/' || c == '=' || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+        }
+
+    }
+
+}
---
+++ WebForum.Tests/Services/AutoMapper/AutoMapperShould.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebForum.Models.Dtos;
+using WebForum.Models;
+using WebForum.Helpers.Mappers;
+
+namespace WebForum.Tests.Services.AutoMapper
+{
+    [TestClass]
+    public class AutoMapperShould
+    {
+        [TestMethod]
+        public void Map_PostToPostShowDto_Success()
+        {
+            // Arrange
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            var mapper = configuration.CreateMapper();
+
+            var post = new Post
+            {
+                Id = 1,
+                Title = "Test Post",
+                Content = "This is a test post."
+            };
+
+            // Act
+            var postShowDto = mapper.Map<PostShowDto>(post);
+
+            // Assert
+            Assert.IsNotNull(postShowDto);
+            //Assert.AreEqual(post.Id, postShowDto.Id);
+            Assert.AreEqual(post.Title, postShowDto.Title);
+            Assert.AreEqual(post.Content, postShowDto.Content);
+        }
+
+    }
+}
